4878aa519d97361bb51ea9a816e5529b3b81cad6
iknityanand <iknityanand@gmail.com>
2019-11-11 13:13:18 +0530

    Controller name Corrected

--- src/main/java/io/github/iknityanand/openshiftapp/web/WelcomeController.java
+++ src/main/java/io/github/iknityanand/openshiftapp/web/WelcomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class WelcomeController {
 
 
     @RequestMapping("/greeting")
