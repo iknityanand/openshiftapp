62ee9cf67cae2df105a209d92927ee23f30666eb
iknityanand <iknityanand@gmail.com>
2019-11-11 12:43:17 +0530

    Controller added

---
+++ src/main/java/io/github/iknityanand/openshiftapp/web/WelcomeController.java
@@ -0,0 +1,15 @@
+package io.github.iknityanand.openshiftapp.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return "Hello Gotham";
+    }
+}
